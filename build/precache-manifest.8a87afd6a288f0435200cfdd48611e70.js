20a5cd4ab3dbdc62b0cf96c23fa0d1b377b74942
karolinadebowska <41170904+karolinadebowska@users.noreply.github.com>
2020-03-24 15:46:20 -0400

    Add files via upload

---
+++ build/precache-manifest.8a87afd6a288f0435200cfdd48611e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc1356cf023c0c96fe738ae802093bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26856765e37117af79a6",
+    "url": "/static/css/main.5b5f1362.chunk.css"
+  },
+  {
+    "revision": "a40c3c5b16aadd2f7549",
+    "url": "/static/js/2.2496f845.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2496f845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26856765e37117af79a6",
+    "url": "/static/js/main.aba20f67.chunk.js"
+  },
+  {
+    "revision": "1efa9debaef5ea33d2f4",
+    "url": "/static/js/runtime-main.b4b3a73d.js"
+  }
+]);
